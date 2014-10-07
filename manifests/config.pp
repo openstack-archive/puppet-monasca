415e03ab59770e60ad4efd2a9a00a158d1a9ff6d
Ryan Bak <ryan.bak@twcable.com>
2014-10-07 10:21:29 -0600

    Added files to generate api and agent configs
    Change-Id: Idda19395ce72863331c1f81118699457f24b1bdb

---
+++ manifests/config.pp
@@ -0,0 +1,45 @@
+# == Class: monasca::config
+#
+# This class is used to manage arbitrary monasca configurations.
+#
+# === Parameters
+#
+# [*xxx_config*]
+#   (optional) Allow configuration of arbitrary monasca configurations.
+#   The value is an hash of xxx_config resources. Example:
+#   { 'DEFAULT/foo' => { value => 'fooValue'},
+#     'DEFAULT/bar' => { value => 'barValue'}
+#   }
+#
+#   In yaml format, Example:
+#   xxx_config:
+#     DEFAULT/foo:
+#       value: fooValue
+#     DEFAULT/bar:
+#       value: barValue
+#
+# [**monasca_config**]
+#   (optional) Allow configuration of monasca.conf configurations.
+#
+# [**monasca_ini**]
+#   (optional) Allow configuration of monasca.ini configurations.
+#
+# [**agent_conf**]
+#   (optional) Allow configuration of monasca.ini configurations.
+#
+#   NOTE: The configuration MUST NOT be already handled by this module
+#   or Puppet catalog compilation will fail with duplicate resources.
+#
+class monasca::config (
+  $monasca_config         = {},
+  $monasca_ini  = {},
+  $agent_config  = {},
+) {
+  validate_hash($monasca_config)
+  validate_hash($monasca_ini)
+  validate_hash($agent_config)
+
+  create_resources('monasca_config', $monasca_config)
+  create_resources('monasca_ini', $monasca_ini)
+  create_resources('agent_config', $agent_config)
+}
