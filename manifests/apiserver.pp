abc3d007ce66e1bf8ed6b9464b51f601b84bf019
bklei <bradley.klein@twcable.com>
2014-10-24 10:46:24 -0600

    Require install parms -- get from hiera/profile
    Change-Id: I0ba94c0cad2d8a6f5f216db75a37fef506a62a33
    Issue: CIRRUS-1325

--- manifests/apiserver.pp
+++ manifests/apiserver.pp
@@ -1,65 +1,78 @@
 #
 # Class for the monasca api server
 #
-# TODOS: remove /tmp/*deb
-#        pull values from hiera
-#
 class monasca::apiserver(
-  $blobmirror = 'bfd01-blobmirror-001.os.cloud.twc.net',
-  $mon_api_build_ver = 'current',
-  $mon_pers_build_ver = 'current',
-  $mon_thresh_build_ver = 'current',
-  $mon_api_deb = 'monasca-api-0.1.0-1414108431485-c6802b.deb',
-  $mon_pers_deb = 'monasca-persister-1.0-SNAPSHOT-1414094614742-fbf81b.deb',
-  $mon_thresh_deb = 'monasca-thresh-1.0.0-SNAPSHOT-1414094648581-3cfa7c.deb',
+  $blobmirror = undef,
+  $mon_api_build_ver = undef,
+  $mon_pers_build_ver = undef,
+  $mon_thresh_build_ver = undef,
+  $mon_api_deb = undef,
+  $mon_pers_deb = undef,
+  $mon_thresh_deb = undef,
 ){
-  ensure_resource('package', 'openjdk-7-jdk', { ensure => 'present' })
+  ensure_resource('package', 'openjdk-7-jre', { ensure => 'present' })
 
   $api_fetch_url = "http://${blobmirror}/repos/monasca/monasca_api"
   $pers_fetch_url = "http://${blobmirror}/repos/monasca/monasca_persister"
   $thresh_fetch_url = "http://${blobmirror}/repos/monasca/monasca_thresh"
-
-  package { 'openjdk-7-jre':
-    ensure => present,
-    before => Package['install-monasca-api'],
-  }
+  $latest_api_deb = "/tmp/${mon_api_deb}"
+  $latest_pers_deb = "/tmp/${mon_pers_deb}"
+  $latest_thresh_deb = "/tmp/${mon_thresh_deb}"
 
   wget::fetch { "${api_fetch_url}/${mon_api_build_ver}/${mon_api_deb}":
-    destination => "/tmp/${mon_api_deb}",
-    timeout     => 120,
-    before      => Package['install-monasca-api'],
+    destination => $latest_api_deb,
+    timeout     => 300,
+    before      => [Package['install-api'],File[$latest_api_deb]],
   }
 
   wget::fetch { "${pers_fetch_url}/${mon_pers_build_ver}/${mon_pers_deb}":
-    destination => "/tmp/${mon_pers_deb}",
-    timeout     => 120,
-    before      => Package['install-monasca-persister'],
+    destination => $latest_pers_deb,
+    timeout     => 300,
+    before      => [Package['install-persister'], File[$latest_pers_deb]],
   }
 
   wget::fetch { "${thresh_fetch_url}/${mon_thresh_build_ver}/${mon_thresh_deb}":
-    destination => "/tmp/${mon_thresh_deb}",
-    timeout     => 120,
-    before      => Package['install-monasca-thresh'],
+    destination => $latest_thresh_deb,
+    timeout     => 300,
+    before      => [Package['install-thresh'], File[$latest_thresh_deb]],
+  }
+
+  file { $latest_api_deb:
+    ensure => present,
+  }
+
+  file { $latest_pers_deb:
+    ensure => present,
+  }
+
+  file { $latest_thresh_deb:
+    ensure => present,
   }
 
   package { 'monasca-api':
     ensure   => latest,
     provider => dpkg,
-    source   => "/tmp/${mon_api_deb}",
-    alias    => 'install-monasca-api',
+    source   => $latest_api_deb,
+    alias    => 'install-api',
   }
 
   package { 'monasca-persister':
     ensure   => latest,
     provider => dpkg,
-    source   => "/tmp/${mon_pers_deb}",
-    alias    => 'install-monasca-persister',
+    source   => $latest_pers_deb,
+    alias    => 'install-persister',
   }
 
   package { 'monasca-thresh':
     ensure   => latest,
     provider => dpkg,
-    source   => "/tmp/${mon_thresh_deb}",
-    alias    => 'install-monasca-thresh',
+    source   => $latest_thresh_deb,
+    alias    => 'install-thresh',
+  }
+
+  # Remove any old debs (puppet won't delete current resources)
+  tidy { '/tmp':
+    matches => 'monasca*.deb',
+    recurse => true,
   }
 }
