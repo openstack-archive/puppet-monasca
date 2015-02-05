aad70f56eaff71bc8fa8005423d5d74e3c38147f
Ryan Bak <ryan.bak@twcable.com>
2015-02-05 13:47:50 -0700

    Updated config for 0.1.19 agent
    Change-Id: I7d68be992a27730f71953afddfc60563cfa7ab99

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -8,35 +8,42 @@
 #   TODO:add comments here.
 #
 class monasca::agent(
-  $enabled                = true,
+  $enabled                 = true,
   $url,
   $username,
   $password,
   $keystone_url,
   $service,
-  $project_name           = 'services',
-  $hostname               = undef,
-  $dimensions             = undef,
-  $recent_point_threshold = '30',
-  $use_mount              = 'no',
-  $listen_port            = '17123',
-  $non_local_traffic      = 'no',
-  $monstatsd_port         = '8125',
-  $monstatsd_interval     = '10',
-  $monstatsd_normalize    = 'yes',
-  $statsd_forward_host    = undef,
-  $statsd_forward_port    = '8125',
-  $device_blacklist_re    = '.*\/dev\/mapper\/lxc-box.*',
-  $log_level              = 'INFO',
-  $collector_log_file     = '/var/log/monasca/agent/collector.log',
-  $forwarder_log_file     = '/var/log/monasca/agent/forwarder.log',
-  $monstatsd_log_file     = '/var/log/monasca/agent/monstatsd.log',
-  $log_to_syslog          = 'yes',
-  $syslog_host            = undef ,
-  $syslog_port            = undef,
-  $virtual_env            = '/var/www/monasca-agent',
-  $agent_user             = 'monasca-agent',
-  $agent_version          = 'latest',
+  $project_name            = 'services',
+  $hostname                = undef,
+  $dimensions              = 'None',
+  $ca_file                 = undef,
+  $max_buffer_size         = '1000',
+  $backlog_send_rate       = '1000',
+  $amplifier               = '0',
+  $recent_point_threshold  = '30',
+  $use_mount               = 'no',
+  $listen_port             = '17123',
+  $non_local_traffic       = 'no',
+  $system_metrics          = 'cpu,disk,io,load,memory',
+  $device_blacklist_re     = '.*\/dev\/mapper\/lxc-box.*',
+  $ignore_filesystem_types = 'tmpfs,devtmpfs',
+  $statsd_port             = '8125',
+  $statsd_interval         = '10',
+  $statsd_normalize        = 'yes',
+  $statsd_forward_host     = undef,
+  $statsd_forward_port     = '8125',
+
+  $log_level               = 'INFO',
+  $collector_log_file      = '/var/log/monasca/agent/collector.log',
+  $forwarder_log_file      = '/var/log/monasca/agent/forwarder.log',
+  $monstatsd_log_file      = '/var/log/monasca/agent/monstatsd.log',
+  $log_to_syslog           = 'yes',
+  $syslog_host             = undef ,
+  $syslog_port             = undef,
+  $virtual_env             = '/var/www/monasca-agent',
+  $agent_user              = 'monasca-agent',
+  $agent_version           = 'latest',
 ) {
   include monasca
   include monasca::params
@@ -147,63 +154,70 @@ class monasca::agent(
     }
   }
 
-  # if $dimensions {
-  #   agent_config {
-  #     'Main/dimensions' : value => $dimensions;
-  #   }
-  # }
-  # else {
-  #   agent_config {
-  #     'Main/dimensions' : ensure => absent;
-  #   }
-  # }
-
-  if $statsd_forward_host {
+  if $ca_file {
     agent_config {
-      'Main/statsd_forward_host' : value => $statsd_forward_host;
-      'Main/statsd_forward_port' : value => $statsd_forward_port;
+      'Api/insecure' : value => false;
+      'Api/ca_file'  : value => $ca_file;
     }
   }
   else {
     agent_config {
-      'Main/statsd_forward_host' : ensure => absent;
-      'Main/statsd_forward_port' : ensure => absent;
+      'Api/insecure' : value  => true;
+      'Api/ca_file'  : ensure => absent;
+    }
+  }
+
+  if $statsd_forward_host {
+    agent_config {
+      'Statsd/monasca_statsd_forward_host'        : value => $statsd_forward_host;
+      'Statsd/monasca_statsd_statsd_forward_port' : value => $statsd_forward_port;
+    }
+  }
+  else {
+    agent_config {
+      'Statsd/monasca_statsd_forward_host'        : ensure => absent;
+      'Statsd/monasca_statsd_statsd_forward_port' : ensure => absent;
     }
   }
 
   if $syslog_host and $syslog_port {
     agent_config {
-      'Main/syslog_host' : value => $syslog_host;
-      'Main/syslog_port' : value => $syslog_port;
+      'Logging/syslog_host' : value => $syslog_host;
+      'Logging/syslog_port' : value => $syslog_port;
     }
   }
   else {
     agent_config {
-      'Main/syslog_host' : ensure => absent;
-      'Main/syslog_port' : ensure => absent;
+      'Logging/syslog_host' : ensure => absent;
+      'Logging/syslog_port' : ensure => absent;
     }
   }
 
   agent_config {
-    'Api/url':                     value => $url;
-    'Api/username':                value => $username;
-    'Api/password':                value => $password;
-    'Api/keystone_url':            value => $keystone_url;
-    'Api/project_name':            value => $project_name;
-    'Main/recent_point_threshold': value => $recent_point_threshold;
-    'Main/use_mount':              value => $use_mount;
-    'Main/listen_port':            value => $listen_port;
-    'Main/additional_checksd':     value => $additional_checksd;
-    'Main/non_local_traffic':      value => $non_local_traffic;
-    'Main/monstatsd_port':         value => $monstatsd_port;
-    'Main/monstatsd_interval':     value => $monstatsd_interval;
-    'Main/monstatsd_normalize':    value => $monstatsd_normalize;
-    'Main/device_blacklist_re':    value => $device_blacklist_re;
-    'Main/log_level':              value => $log_level;
-    'Main/collector_log_file':     value => $collector_log_file;
-    'Main/forwarder_log_file':     value => $forwarder_log_file;
-    'Main/monstatsd_log_file':     value => $monstatsd_log_file;
-    'Main/log_to_syslog':          value => $log_to_syslog;
-    'Main/dimensions':             value => "service:${service}"
+    'Api/url':                         value => $url;
+    'Api/username':                    value => $username;
+    'Api/password':                    value => $password;
+    'Api/keystone_url':                value => $keystone_url;
+    'Api/project_name':                value => $project_name;
+    'Api/max_buffer_size':             value => $max_buffer_size;
+    'Api/backlog_send_rate':           value => $backlog_send_rate;
+    'Api/amplifier':                   value => $amplifier;
+    'Main/dimensions' :                value => $dimensions;
+    'Main/recent_point_threshold':     value => $recent_point_threshold;
+    'Main/use_mount':                  value => $use_mount;
+    'Main/listen_port':                value => $listen_port;
+    'Main/additional_checksd':         value => $additional_checksd;
+    'Main/non_local_traffic':          value => $non_local_traffic;
+    'Main/system_metrics':             value => $system_metrics;
+    'Main/device_blacklist_re':        value => $device_blacklist_re;
+    'Main/ignore_filesystem_types':    value => $ignore_filesystem_types;
+    'Statsd/monasca_statsd_port':      value => $statsd_port;
+    'Statsd/monasca_statsd_interval':  value => $statsd_interval;
+    'Statsd/monasca_statsd_normalize': value => $statsd_normalize;
+    'Logging/log_level':               value => $log_level;
+    'Logging/collector_log_file':      value => $collector_log_file;
+    'Logging/forwarder_log_file':      value => $forwarder_log_file;
+    'Logging/monstatsd_log_file':      value => $monstatsd_log_file;
+    'Logging/log_to_syslog':           value => $log_to_syslog;
   }
 }
