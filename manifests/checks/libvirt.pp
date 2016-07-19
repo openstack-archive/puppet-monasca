4b36f0a080eaef518b5ca764723368d464a49abf
Brad Klein <bradley.klein@twcable.com>
2016-07-19 15:41:09 -0600

    Update libvirt plugin configuration for latest agent.
    Also start to conform to puppet-openstack standard parameter
    documentations and parameter defaults.
    
    Change-Id: I3d0417840e9a293cd9379f5f9ddf6686f4d77ed1

--- manifests/checks/libvirt.pp
+++ manifests/checks/libvirt.pp
@@ -5,53 +5,111 @@
 #
 # === Parameters
 #
-# [*admin_user*]
-#   name of the monasca admin
-#
 # [*admin_password*]
-#   password for the monasca admin
+#   (Required) Password for the monasca admin.
 #
 # [*admin_tenant_name*]
-#   name of the monasca admin tenant/project
+#   (Required) Name of the monasca admin tenant/project.
 #
-# [*identity_uri*]
-#   uri of the keystone instance
+# [*admin_user*]
+#   (Required) Name of the monasca admin.
 #
-# [*metadata*]
-#   a list of instance metadata to be submitted as dimensions with service data.
-#
-# [*customer_metadata*]
-#   a list of instance metadata to be submitted as dimensions with cusomter data.
-#
-# [*region_name*]
-#   openstack keystone region for this install
-#
-# [*network_use_bits*]
-#   submit network metrics in bits rather than bytes
+# [*alive_only*]
+#   (Optional) Will suppress all per-VM metrics aside from host_alive_status
+#   and vm.host_alive_status, including all I/O, network, memory, ping, and
+#   CPU metrics.  Aggregate metrics, however, would still be enabled if alive_only
+#   is true.
+#   Defaults to false.
 #
 # [*cache_dir*]
-#   Cache directory to persist data.
-# [*vm_probation*]
-#   Period of time (in seconds) in which to suspend metrics from a newly-created VM.
-#   This is to prevent quickly-obsolete metrics in an environment with a high amount
-#   of instance churn.
+#   (Optional) Cache directory to persist data.
+#   Defaults to '/dev/shm'.
+#
+# [*customer_metadata*]
+#   (Optional) A list of instance metadata to be submitted as dimensions
+#   with customer data.
+#   Defaults to not set in the config file.
+#
+# [*disk_collection_period*]
+#   (Optional) Have disk metrics be outputted  less often to reduce
+#   metric load on the system. If this is less than the agent collection
+#   period, it will be ignored.
+#   Defaults to 0.
+#
+# [*identity_uri*]
+#   (Required) URI of the keystone instance.
+#
+# [*metadata*]
+#   (Optional) A list of instance metadata to be submitted as dimensions
+#   with service data.
+#   Defaults to not set in the config file.
+#
+# [*network_use_bits*]
+#   (Optional) Submit network metrics in bits rather than bytes.
+#   Defaults to true.
+#
 # [*nova_refresh*]
-#   Interval to force data refresh.  Set to 0 to refresh every time the
-#   Collector runs, or to None to disable regular refreshes entirely (though
+#   (Optional) Interval to force data refresh.  Set to 0 to refresh every time
+#   the collector runs, or to None to disable regular refreshes entirely (though
 #   the instance cache will still be refreshed if a new instance is detected).
+#   Defaults to 14400 (4 hours).
+#
+# [*ping_check*]
+#   (Optional) The entire command line (sans the IP address, which is automatically
+#   appended) used to perform a ping check against instances, with a keyword NAMESPACE
+#   automatically replaced with the appropriate network namespace for the VM being
+#   monitored.  Set to false to disable ping checks.
+#   Defaults to false.
+#
+# [*region_name*]
+#   (Required) Openstack keystone region for this install.
+#
+# [*vm_cpu_check_enable*]
+#   (Optional) Enables collecting of VM CPU metrics.
+#   Defaults to true.
+#
+# [*vm_disks_check_enable*]
+#   (Optional) Enables collecting of VM disk metrics.
+#   Defaults to true.
+#
+# [*vm_extended_disks_check_enable*]
+#   (Optional) nable collecting of extended disk metrics.
+#   Defaults to false.
+#
+# [*vm_network_check_enable*]
+#   (Optional) Enables collecting of VM network metrics.
+#   Defaults to true.
+#
+# [*vm_ping_check_enable*]
+#   (Optional) Enables host alive ping check.
+#   Defaults to false.
+#
+# [*vm_probation*]
+#   (Optional) Period of time (in seconds) in which to suspend metrics
+#   from a newly-created VM.  This is to prevent quickly-obsolete metrics
+#   in an environment with a high amount of instance churn.
+#   Defaults to 300 seconds.
 #
 class monasca::checks::libvirt(
-  $admin_user,
-  $admin_password,
-  $admin_tenant_name,
-  $identity_uri,
-  $metadata          = undef,
-  $customer_metadata = undef,
-  $region_name       = undef,
-  $network_use_bits  = undef,
-  $cache_dir         = '/dev/shm',
-  $vm_probation      = '300',
-  $nova_refresh      = '14400'
+  $admin_password                 = undef,
+  $admin_tenant_name              = undef,
+  $admin_user                     = undef,
+  $alive_only                     = false,
+  $cache_dir                      = '/dev/shm',
+  $customer_metadata              = [],
+  $disk_collection_period         = 0,
+  $identity_uri                   = undef,
+  $metadata                       = [],
+  $network_use_bits               = true,
+  $nova_refresh                   = '14400',
+  $ping_check                     = false,
+  $region_name                    = undef,
+  $vm_cpu_check_enable            = true,
+  $vm_disks_check_enable          = true,
+  $vm_extended_disks_check_enable = false,
+  $vm_network_check_enable        = true,
+  $vm_ping_check_enable           = false,
+  $vm_probation                   = '300',
 ){
   $conf_dir = $::monasca::agent::conf_dir
 
